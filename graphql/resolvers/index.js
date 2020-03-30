f9f500570d0cc85b252befc58d8d5a3f45ec13d2
anirudhmana <amana@oes.edu.au>
2020-03-30 12:24:00 +1100

    Add helper functions for date

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,15 +2,28 @@ const bcrypt = require("bcryptjs");
 const Event = require("../../models/event");
 const User = require("../../models/users");
 const Booking = require("../../models/booking");
+const { dateToString } = require("../../helpers/date");
 
 const transformEvent = event => {
 	return {
 		...event._doc,
 		_id: event.id,
-		date: new Date(event._doc.date).toISOString(),
+		date: dateToString(event._doc.date),
 		creator: user.bind(this, event.creator)
-	};;
+	};
 };
+
+const transformBooking = booking =>{
+return {
+	...booking._doc,
+	_id: booking.id,
+	user: user.bind(this, booking._doc.user),
+	event: singleEvent.bind(this, booking._doc.event),
+	createdAt: dateToString(booking._doc.createdAt),
+	updatedAt: dateToString(booking._doc.updatedAt)
+}
+	
+}
 
 const event = async eventIds => {
 	try {
@@ -67,14 +80,7 @@ module.exports = {
 		try {
 			const bookings = await Booking.find();
 			return bookings.map(booking => {
-				return {
-					...booking._doc,
-					_id: booking.id,
-					user: user.bind(this, booking._doc.user),
-					event: singleEvent.bind(this, booking._doc.event),
-					createdAt: new Date(booking._doc.createdAt).toISOString(),
-					updatedAt: new Date(booking._doc.updatedAt).toISOString()
-				};
+				return transformBooking(booking);
 			});
 		} catch (err) {
 			throw err;
@@ -149,14 +155,7 @@ module.exports = {
 			event: fetchedEvent
 		});
 		const result = await booking.save();
-		return {
-			...result._doc,
-			_id: result.id,
-			user: user.bind(this, booking._doc.user),
-			event: singleEvent.bind(this, booking._doc.event),
-			createdAt: new Date(result._doc.createdAt).toISOString(),
-			updatedAt: new Date(result._doc.updatedAt).toISOString()
-		};
+		return transformBooking(result);
 	},
 	cancelBooking: async args => {
 		try {
